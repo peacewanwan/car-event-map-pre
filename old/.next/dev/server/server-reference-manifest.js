e9b7f9b53c4d8201f80f94d06d21c85e77814b8e
白須剛生 <takeomba@shirasutakeonoMacBook-Air.local>
2026-03-16 22:37:13 +0900

    remove old folder

--- old/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HUOp9nhDvC4DJcI8xKxopqPSbaRUvk7kEDGpho9U0Bg=\"\n}"
--- old/.next/dev/static/chunks/app_page_tsx_a8c03fea._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/components_Map_tsx_1a1804b7._.js",
-  "static/chunks/_693a49f0._.js"
-],
-    source: "dynamic"
-});
